8998d1f30d60b25fff495df3db76c7728d3c3eda
Allen <asd9fgh9@gmail.com>
2020-07-05 22:31:57 +0800

    ES6 module

---
+++ 0626/module.js
@@ -0,0 +1,18 @@
+// export default {
+//     myName: '小明',
+//     fn: function() {
+//         console.log(this.myName);
+//     }
+// }
+// 預設匯出(每個檔案只有一個)
+
+// //具名匯出,大多用在函式庫
+// export function fn() {
+//     console.log('這是具名匯出');
+// }
+
+// export const a = 1;
+
+(function() {
+    console.log('結果')
+})();
